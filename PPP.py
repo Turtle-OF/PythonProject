2a3646776b9f27531384711d5aec8a56e717a89d
Turtle <seniorkir2022@gmail.com>
2025-05-03 10:30:38 +0300

    Les1

--- PPP.py
+++ PPP.py
@@ -1,1 +1,23 @@
-print("Hello")
+def calculator():
+    print("Простий калькулятор")
+    num1 = float(input("Введи перше число: "))
+    operation = input("Операція (+, -, *, /): ")
+    num2 = float(input("Введи друге число: "))
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Ділення на нуль неможливе!"
+    else:
+        return "Невідома операція!"
+
+    return f"Результат: {result}"
+
+print(calculator())
