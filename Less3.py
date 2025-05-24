00e48187014e5cb8a2657b48a9590918ecea9777
Turtle <seniorkir2022@gmail.com>
2025-05-24 12:42:36 +0300

    small calculator fix bugs

---
+++ Less3.py
@@ -0,0 +1,9 @@
+def check_age(age):
+    try:
+        assert age >= 18, "Вам має бути 18 років або більше"
+        print("Ви можете використовувати цей сервіс")
+    except AssertionError as e:
+        print(e)
+
+
+check_age(16)
